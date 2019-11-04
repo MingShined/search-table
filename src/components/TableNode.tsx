7d6fd19af3b0c892172cf842dae4769631cc86d0
mingshined <996578843@qq.com>
2019-11-04 17:31:49 +0800

    update

--- src/components/TableNode.tsx
+++ src/components/TableNode.tsx
@@ -1,9 +1,11 @@
 import { ColumnProps } from 'antd/lib/table';
+import { customColumnProps } from 'src/components/custom-table/utils';
 
 const getColumns = (): ColumnProps<any>[] => {
   return [
     {
       title: 'xx',
+      ...customColumnProps,
       dataIndex: 'xx'
     }
   ];
