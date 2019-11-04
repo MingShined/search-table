c369e15c0fc761b27d6b20f309c7e90b0cafd20b
mingshined <996578843@qq.com>
2019-11-04 17:26:08 +0800

    update

--- src/indexPage.tsx
+++ src/indexPage.tsx
@@ -6,9 +6,33 @@ import React, { Fragment } from 'react';
 import SearchTable from 'src/components/search-table';
 import getFormData from './components/SearchNode';
 import getColumns from './components/TableNode';
+import { connect } from 'dva';
 import PromotionTableActions from './components/TableActions';
 interface Props {}
 const DemoManage: React.FC<Props> = props => {
+  const {
+    dictionaryMap: { brands, itemType }
+  } = props;
+  const filterData = [
+    {
+      key: 'brandIds',
+      label: '品牌',
+      type: 'checkbox',
+      dataSource: brands
+    },
+    {
+      key: 'channels',
+      label: '经销商渠道',
+      type: 'select',
+      dataSource: brands
+    },
+    {
+      key: 'itemType',
+      label: '商品类型',
+      type: 'select',
+      dataSource: itemType
+    }
+  ];
   return (
     <SearchTable
       commonFormProps={{ formData: getFormData() }}
@@ -17,9 +41,7 @@ const DemoManage: React.FC<Props> = props => {
       tableAction={<PromotionTableActions />}
       tabKey="xx"
       ajaxProps={{ url: '/', method: 'GET' }}
-    >
-      1
-    </SearchTable>
+    />
   );
 };
-export default DemoManage;
+export default connect(({ common }) => ({ ...common }))(DemoManage);
